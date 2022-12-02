c6745743fcedec60825805867e91658f28d9c727
Mike Kasberg <kasberg.mike@gmail.com>
2022-12-01 21:53:54 -0700

    2022, Day 01

---
+++ 01/solution.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+lines = ARGF.readlines.map(&:strip)
+
+total_calories = []
+current_calories = 0
+
+lines.each do |l|
+    if l.empty?
+        total_calories << current_calories
+        current_calories = 0
+    else
+        current_calories += l.to_i
+    end
+end
+
+if current_calories != 0
+    total_calories << current_calories
+end
+
+puts "Result: " + total_calories.max.to_s
